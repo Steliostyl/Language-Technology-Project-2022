ac5e3cc4274ce0dbc08dbcbfdad30c0d81996ca4
Steliostyl <steliostylian@gmail.com>
2022-01-18 02:32:22 +0200

    Web Scraping v0.2
    Changed website to CNBC and made adjustments

--- websitescraper/websitescraper/main.py
+++ websitescraper/websitescraper/main.py
@@ -1,35 +1,30 @@
 import json
 
 class Article:
-    def __init__(self, title, details, note):
+    def __init__(self, title, tag, date, author, paragraphs):
         self.title = title
-        self.details = details
-        self.note = note
+        self.tag = tag
+        self.author = author[0]
+        datetime = ''
+        paragraphs_string = ''
+
+        for item in date:
+            datetime = datetime + item + " "
+
+        for item in paragraphs:
+            paragraphs_string = paragraphs_string + item
+
+        self.datetime = datetime
+        self.paragraphs_string = paragraphs_string
+
     def printArticle(self):
-        print("\nTitle:", "\n" + self.title + "\n")
-        print("Details:", "\n" + self.details, "\n" + temp_note[0], "\n" + temp_note[1] + "\n")
-        print(temp_text)
+        print(self.title, "\n" + self.tag, "\n" + self.datetime, "\n" + self.author[0], "\n" + self.paragraphs_string)
 
-file = open('cnn_spider.json')
-init_dict = json.load(file)
-temp_text = []
-temp_details = ''
-temp_note = None
-for a in init_dict:
-    try:
-        temp_text.append(a['text'])
-    except:
-        try:
-            temp_title = a['article_title'][0]
-        except:
-            try:
-                for i in range(0,len(a['story_details'])):
-                    temp_details = temp_details + a['story_details'][i] + "\n"
-            except:
-                temp_note = (a['note_title'][0],a['note_text'][0])
-        
-new_article = Article(temp_title, temp_details, temp_note)
-new_article.printArticle()
+def readJSON(filename):
+    file = open(filename)
+    init_dict = json.load(file)
+    new_article =  Article(init_dict[0]['article_title'], init_dict[0]['article_tag'], init_dict[0]['article_date'], init_dict[0]['author'],init_dict[0]['paragraphs'])
+    file.close()
+    return new_article
 
-#print(type(temp_title))
-#print(temp_title,"\n",temp_details,"\n",temp_note[0],"\n",temp_note[1],"\n",temp_text)
+readJSON("cnn_spider.json").printArticle()
--- websitescraper/websitescraper/spiders/cnn_spider.py
+++ websitescraper/websitescraper/spiders/cnn_spider.py
@@ -3,23 +3,16 @@ import scrapy
 
 class CnnSpider(scrapy.Spider):
     name = 'cnn_spider'
-    start_urls = ['https://edition.cnn.com/interactive/2022/01/world/greenland-denmark-social-experiment-cmd-idnty-intl-cnnphotos/']
+    start_urls = ['https://www.cnbc.com/2022/01/17/credit-suisse-needs-to-salvage-reputation-and-personnel-after-latest-scandal.html']
 
     # response.xpath('//div[@class="d-intro-hed"]/p/span/text()').extract_first()
 
     def parse(self, response):
         #final_text = ''
-        yield { 'article_title': response.xpath('//div[@class="d-intro-hed"]/h1/text()').extract() }
-        yield { 'story_details': response.xpath('//div[@class="d-intro-hed"]/p/span/text()').extract() }
-
-        for text_container in response.xpath('//div[@class="cp-text container"]/p'):
-            note_title = text_container.xpath('strong/em')
-            if note_title:
-                yield {
-                    'note_title': note_title.xpath('text()').extract(),
-                    'note_text': text_container.xpath('em/text()').extract()
-                }
-            else:
-                yield { 
-                    'text': text_container.xpath('text()').extract()
-                }
+        yield { 
+            'article_title': response.xpath('//h1/text()').extract_first(),
+            'article_tag': response.xpath('//a[@class="ArticleHeader-eyebrow"]/text()').extract_first(),
+            'article_date': response.xpath('//time/text()').extract(),
+            'author': response.xpath('//a[@class="Author-authorName"]/text()').extract(),
+            'paragraphs': response.xpath('//div[@class="ArticleBody-articleBody"]/div[@class="group"]//text()').extract()
+        }
