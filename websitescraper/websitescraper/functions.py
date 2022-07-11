abacce854cba810fa7d320da42c4e13d397b36dd
Steliostyl <steliostylian@gmail.com>
2022-07-11 19:28:26 +0300

    Need to make Articles from objects -> dictionaries

--- websitescraper/websitescraper/functions.py
+++ websitescraper/websitescraper/functions.py
@@ -15,7 +15,7 @@ class Article:
         self.pos_tags = [] # List of tagged sentences
 
         # Join the text of the article into 1 string
-        self.paragraphs = ''.join(unprocessed_text)
+        self.paragraphs = ' '.join(unprocessed_text)
 
     def printArticle(self):
         print(self.id)
@@ -32,27 +32,44 @@ class Article:
 def readDateTimeFromString(datetimeString):
     return datetime.fromisoformat(datetimeString[:-2] + ":" + datetimeString[len(datetimeString)-2:])
 
-# Function to read the articles from the JSON file and create Article objects from them
-def readJSON(filename):
+# Function to read the articles from the JSON file that 
+# the scraper createdand create Article objects from them
+def readScraperJSON(filename):
     article_list = []
     file = open(filename)
     init_dict = json.load(file)
+    empty_pars = 0
     for index, article in enumerate(init_dict):
         if len(article['paragraphs']) > 0:
             paragraphs = []
             for paragraph in article['paragraphs']:
                 paragraphs.append(extract_text(paragraph))
-            new_article =  Article(article['article_title'], article['article_tag'], readDateTimeFromString(article['article_datetime']), article['author'],paragraphs,article['url'], index)
+            new_article =  Article(article['article_title'], article['article_tag'], readDateTimeFromString(article['article_datetime']), article['author'],paragraphs,article['url'], index - empty_pars)
             file.close()
             article_list.append(new_article)
+        else:
+            empty_pars += 1
     return article_list
 
 def saveEntryToJSON(article):
-    with open('processed_articles.json', 'a') as outfile:
-        json.dump(article.__dict__, outfile, indent=4, default=str)
+    with open('processed_articles.json', 'a') as file:
+        json.dump(article.__dict__, file, indent=4, default=str)
+        file.write(',')
 
-def deleteFile(filename):
+def createNewJSONforProcessedArticles(filename):
     # Check if the file exists
     if(os.path.isfile(filename)):
         # Remove file
         os.remove(filename)
+    with open('processed_articles.json', 'w') as file:
+        file.write('{\t"articles": [\n')
+    
+
+def readArticleJSON(filename):
+    with open(filename, 'r') as file:
+        articles = json.load(file)["articles"]
+    #print(type(articles[0]))
+    #articles[0].printArticle()
+    return articles
+
+print(readArticleJSON('processed_articles.json')[0]["paragraphs"])
--- websitescraper/websitescraper/main.py
+++ websitescraper/websitescraper/main.py
@@ -1,12 +1,21 @@
 import functions
 import nltk_functions
+import os
 
-articles = functions.readJSON('websitescraper/cnbc_spider_new.json')
-functions.deleteFile('processed_articles.json')
+articles = functions.readScraperJSON('websitescraper/cnbc_spider_new.json')
+functions.createNewJSONforProcessedArticles('processed_articles.json')
+
 
 for article in articles:
     nltk_functions.pos_tag(article)
     #article.printArticle()
+
+with open('processed_articles.json', 'rb+') as file:
+    file.seek(-1, os.SEEK_END)
+    file.truncate()
+
+with open('processed_articles.json', 'a') as file:
+    file.write('\n]\n}')
 
 #print(type(articles[0].pos_tags))
 #print(articles[0].paragraphs)
