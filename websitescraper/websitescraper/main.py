9c8e61474df712305db951fa0823171b4ef9c1a1
Steliostyl <steliostylian@gmail.com>
2022-07-13 17:39:09 +0300

    Same commit as last one

--- websitescraper/websitescraper/main.py
+++ websitescraper/websitescraper/main.py
@@ -7,14 +7,10 @@ articles = functions.readScraperJSON('cnbc_spider_new.json')
 for article in articles:
     nltk_functions.pos_tag(article)
     
-functions.createNewJSON('processed_articles.json')
 functions.saveArticlesToJSON(articles, 'processed_articles.json')
-functions.cleanupJSONfile('processed_articles.json')
 
 articles_from_JSON = functions.readArticlesFromJSON('processed_articles.json')
 nltk_functions.filter_stop_words_2(articles_from_JSON)
 
-functions.createNewJSON('no_stop_words.json')
 functions.saveArticlesToJSON(articles_from_JSON, 'no_stop_words.json')
-functions.cleanupJSONfile('no_stop_words.json')
 #print(articles_from_JSON[0]['pos_tags_no_stopwords'])
--- websitescraper/websitescraper/spiders/cnbc_spider.py
+++ websitescraper/websitescraper/spiders/cnbc_spider.py
@@ -1,5 +1,5 @@
 import scrapy
-from scrapy.crawler import CrawlerProcess
+#from scrapy.crawler import CrawlerProcess
 
 class CnbcSpider(scrapy.Spider):
     name = 'cnbc_spider'
@@ -10,20 +10,16 @@ class CnbcSpider(scrapy.Spider):
             yield response.follow(link.get(), callback=self.parse_article)
     
     def parse_article(self, response):
-        yield { 
+        par = response.xpath('//div[@class="ArticleBody-articleBody"]/div[@class="group"]/p').extract()
+        if par == []:
+            return
+
+        article = { 
             'article_title': response.xpath('//h1/text()').extract_first(),
             'article_tag': response.xpath('//a[@class="ArticleHeader-eyebrow"]/text()').extract_first(),
             'author': response.xpath('//div[@class="Author-authorNameAndSocial"]//text()').extract_first(),
             'article_datetime': response.css('time::attr(datetime)').get(),
-            'paragraphs': response.xpath('//div[@class="ArticleBody-articleBody"]/div[@class="group"]/p').extract(),
+            'paragraphs': par,
             'url': response.url
         }
-
-
-if __name__ == "__main__":
-    process = CrawlerProcess({
-        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-    })
-
-    process.crawl(CnbcSpider)
-    process.start() # the script will block here until the crawling is finished
+        return article
