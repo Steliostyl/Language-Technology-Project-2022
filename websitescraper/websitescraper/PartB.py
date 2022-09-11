0aa74e149d3960f5c746b0f92019538a80ce2733
Steliostyl <steliostylian@gmail.com>
2022-09-11 22:46:43 +0300

    Finished Part B

--- websitescraper/websitescraper/PartB.py
+++ websitescraper/websitescraper/PartB.py
@@ -23,18 +23,19 @@ def vectorize_Documents(dataset):
 
 def train_Classifier(d_categories, tf_idf_vector):
     """Trains a model to classify categories using their feature vector"""
-    model = MultinomialNB().fit(tf_idf_vector, d_categories)
-    return model
+    t_classifier = MultinomialNB().fit(tf_idf_vector, d_categories)
+    return t_classifier
 
-def predict_Category(documents, classifier):
+def predict_Category_Clf(documents, classifier, print=False):
     """Predicts the category of a list of documents"""
 
     features_count = count_Vectorizer.transform(documents)
     features_tfidf = tf_idf_Transformer.transform(features_count)
     predicted = classifier.predict(features_tfidf)
-
-    for doc, category in zip(documents, predicted):
-        print('%r\n%s\n' % (doc, twenty_news_train.target_names[category]))
+    
+    if(print):
+        for doc, category in zip(documents, predicted):
+            print('%r\n%s\n' % (doc, twenty_news_train.target_names[category]))
 
 # Read dataset and load it into variable
 twenty_news_train = fetch_20newsgroups(subset='train', shuffle=True, random_state=21)
@@ -45,11 +46,12 @@ tf_idf_vect = vectorize_Documents(twenty_news_train)
 # Train classifier to later predict article category
 classifier = train_Classifier(twenty_news_train.target, tf_idf_vect)
 
-# Predict category of a random document from our dataset
+# Predict category of test documents from the dataset
 twenty_news_test = fetch_20newsgroups(subset='test', shuffle=True, random_state=21)
-predict_Category(twenty_news_test.data, classifier)
+predict_Category_Clf(twenty_news_test.data, classifier)
+#predict_Category_custom(twenty_news_test.data, tf_idf_vect)
 
 # Measure the performance of model
 finish_time = perf_counter()
 e_time = finish_time - start_time
-print("Elapsed time:", e_time)
+print("Articles:", len(twenty_news_test.data), "\nElapsed time:", e_time)
